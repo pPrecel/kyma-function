b90da4f7fd5891a5e61af6b51d7c41a84c534d31
pPrecel <filip.strozik@outlook.com>
2021-08-20 13:20:28 +0200

    Add missing configurations

--- hack/deployall.sh
+++ hack/deployall.sh
@@ -2,13 +2,24 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# create namespace
 kubectl create namespace "kyma-functions"
 
+# apply secrets
+FILES="${SCRIPTPATH}/private/*"
+for f in $FILES
+do
+  echo "Applying $(basename $f) file..."
+  kubectl apply -f $f
+  echo ""
+done
+
+# apply functions and repos
 FILES="${SCRIPTPATH}/configs/*"
 for f in $FILES
 do
   echo "Processing $(basename $f) file..."
-  kyma apply function -f $f
+  kyma-dev apply function -f $f
   echo ""
 done
 
