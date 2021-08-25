9d2e90ab42459d7fbeb2d93913cb3fbe0b227bf6
pPrecel <filip.strozik@outlook.com>
2021-08-25 08:21:08 +0200

    Add commits

--- hack/deployall.sh
+++ hack/deployall.sh
@@ -3,25 +3,32 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # create namespace
-kubectl create namespace "kyma-functions"
+kubectl create namespace "kyma-functions" &2>/dev/null
 echo ""
 
 # apply secrets
+SECRETS=0
 FILES="${SCRIPTPATH}/private/*"
 for f in $FILES
 do
   echo "Applying $(basename $f) file..."
   kubectl apply -f $f
   echo ""
+
+  SECRETS=$( expr $SECRETS + 1 )
 done
 
 # apply functions and repos
+FUNCS=0
 FILES="${SCRIPTPATH}/configs/*"
 for f in $FILES
 do
   echo "Processing $(basename $f) file..."
   kyma-dev apply function -f $f
   echo ""
+
+  FUNCS=$( expr $FUNCS + 1 )
 done
 
-echo "done"
+echo "Applyed $FUNCS functions and $SECRETS secrets..."
+echo "Done"
